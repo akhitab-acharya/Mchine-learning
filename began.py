3c2830d63c2396e30f6b7ad64372f510cc3f55cf
akhitab-acharya <58125309+akhitab-acharya@users.noreply.github.com>
2019-12-08 06:18:41 +0530

    4. Accuracy test
    4. Accuracy test

--- began.py
+++ began.py
@@ -49,3 +49,23 @@ predictions = model.predict([[24,1], [22,0]])
 predictions
 
 ----------------------------------------------------
+4. Accuracy test
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+music_data = pd.read_csv('ML_music.csv')
+x = music_data.drop(columns = ['Genre']) #x is i/p data set
+y = music_data['Genre'] # y is o/p data set
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+predictions = model.predict(x_test)
+
+score = accuracy_score(y_test, predictions)
+score
+
+------------------------------------------------
